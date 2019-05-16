5e789306075908de859c67450cd90d752fb2fb1b
RegisYamamoto <regisyamamoto@gmail.com>
2019-05-16 09:17:08 -0300

    primeiro commit

---
+++ src/com/regis/TesteAutomatizado.java
@@ -0,0 +1,22 @@
+package com.regis;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteAutomatizado {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com.br");
+		
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("Caelum");
+		
+		campoDeTexto.submit();
+		
+	}
+
+}
